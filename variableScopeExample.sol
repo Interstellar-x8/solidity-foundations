3b1dacad127a3234934035c595aeba9b8922f274
Interstellar-x8 <interstellar.x8@gmail.com>
2025-05-27 16:51:45 -0400

    adding variableScopeExample file

---
+++ variableScopeExample.sol
@@ -0,0 +1,25 @@
+pragma solidity >= 0.5.0 < 0.9.0;
+
+// Variable Scope = Functions & Variables.
+
+// Private = You can call the function inside the contract.
+// Internal = Called only within the contract OR other contracts that inherit the 
+// Internal is slightly less restrictive than public.
+// External = You can call the function outside the contract not from other functions.
+// Public = You can call the function from outside the smart contract as well as inside the contract.
+// Rule of Thumb = Give the minimum amount of privilege to any entity. 
+// 1. Private, 2. Internal, 3. External, 4. Public.
+
+contract C {
+    uint public data = 10; // state variable
+
+    function x() public pure returns(uint) {
+        // modify fuction x so that you return 30 for data without changing the data of the state variable.
+        uint newData = 30;
+        return newData;
+    }
+
+    function y() public view returns (uint) {
+        return data;
+    }
+}
