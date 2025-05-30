f7094f0f6ef00926a5ff402e4eaea12e79f9d7d7
Interstellar-x8 <interstellar.x8@gmail.com>
2025-05-30 08:52:43 -0400

    adding loops mod

--- loops.sol
+++ loops.sol
@@ -4,7 +4,27 @@ pragma solidity >=0.7.0 < 0.9.0;
 // For example, if we want to check the grade of every student in that class, we loop from 1 to that number.
 
 contract loop {
-    function checkMultiples(uint _num, uint _nums) public view returns(bool) {
+
+    // a list of numbers ranging from 1 to 10
+    uint [] public numbersList = [1,2,3,4,5,6,7,8,9,10];
+
+    function checkMultiples(uint _number) public view returns (uint) {
+        uint count = 0;
+        // i is the index of each iteration
+        // 1. initialize start of loop
+        // 2. determine the length of loop.
+        // 3. direct the index after each turn.
+        for(uint i=1; i < numbersList.length; i++) {
+            // logic for the loop
+            if(checkMultipleValidity(numbersList[i], _number)) {
+                count ++;
+            }
+        }
+        return count;
+    }
+
+
+    function checkMultipleValidity(uint _num, uint _nums) public view returns(bool) {
         // modulo operator =  %
         if(_num % _nums == 0) {
             return true;
@@ -13,3 +33,13 @@ contract loop {
         }
     }
 }
+
+/* Exercise: Create a contract myLoopingPracticesContract and place all the following code within:
+
+1. Create a list that ranges from 1 to 20 called longList 
+2. Create a list called numbersList of the following numbers: 1, 4, 34, 56
+3. Create a function that loops through numbersList and returns a true value if the number
+that the user inputs exists in the list, otherwise it should return false.
+4. Create a function that loops through and returns how many even numbers there are in the long list
+
+*/
