1d91e5683614e064dd8b3cd62a4b1d272d135a8a
Interstellar-x8 <interstellar.x8@gmail.com>
2025-06-02 16:22:48 -0400

    myLoopingContract file add.

--- loops.sol
+++ loops.sol
@@ -43,3 +43,32 @@ that the user inputs exists in the list, otherwise it should return false.
 4. Create a function that loops through and returns how many even numbers there are in the long list
 
 */
+
+contract myLoopingPracticeContract {
+    // arrays
+    uint [] longList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    uint [] numbersList = [1,4,34,56];
+
+    function numbersListLoop(uint userNumber) public view returns(bool) {
+            bool numberExists = false;
+        for(uint i = 0; i < numbersList.length; i++) {
+            if(numbersList[i] == userNumber) {
+                numberExists = true;
+            }
+        }
+        return numberExists;
+    }
+
+
+// always going to be an even number % 2 == 0
+
+function evenNumbersLoop() public view returns(uint) {
+    uint count = 0;
+    for(uint i = 0; i < longList.length; i ++) {
+        if(longList[i] % 2==0) {
+            count ++;
+        }
+    }
+    return count;
+ }
+}
