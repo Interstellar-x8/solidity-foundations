2fabe6855321d05ef858d3e5c77fd94bf7987437
Interstellar-x8 <interstellar.x8@gmail.com>
2025-06-10 08:17:15 -0400

    adding exercise to array file.

--- arrays.sol
+++ arrays.sol
@@ -35,6 +35,31 @@ contract learnArrays {
         delete myArray[i]; 
     }
 
+    /* Exercise:
+        1. Create an empty array called changeArray
+        2. Create a function called removeElement which sets the index argument of the array to the last element in the array
+        3. Remove the last index from that function with the pop method
+        4. Create a function called test which pushes 1 2 3 4 into change array
+        5. Remove the element 2 from the array when the contract is called
+    */ 
+
+    uint [] public changeArray;
+
+    function removeElement(uint i) public {
+        changeArray[i] = changeArray[changeArray.length -1];
+        changeArray.pop();
+    }
+
+    function test() public {
+        for(uint i = 1; i <= 4; i++) {
+            changeArray.push(i);
+        }
+    }
+
+    function getChangeArray() public view returns(uint[] memory) {
+        return changeArray;
+    }
+
 }
 
     
