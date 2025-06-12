9d45d81ab4ebdfaab112e66382bfa85fd5619397
Interstellar-x8 <interstellar.x8@gmail.com>
2025-06-12 08:07:07 -0400

    structs add.

---
+++ structs.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.7.0 < 0.9.0;
+
+// structs are types that used to represent a record in Solidity. 
+
+contract learnStructs {
+    struct Movie {
+        string title;
+        string director;
+        uint movie_id; 
+    }
+
+    // Movie = Data Type, movie = variable
+    Movie movie; 
+
+    function setMovie() public {
+        movie = Movie('Blade Runner', 'Ridley Scott', 1);
+    }
+
+    function getMovieId() public view returns(uint) {
+        return movie.movie_id;
+    }
+}
