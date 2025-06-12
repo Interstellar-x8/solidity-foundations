68737b5b554af5cc14942812c5b2cceb977d4794
Interstellar-x8 <interstellar.x8@gmail.com>
2025-06-12 07:57:13 -0400

    mod enum.

--- enums.sol
+++ enums.sol
@@ -6,19 +6,39 @@ pragma solidity >= 0.7.0 < 0.9.0;
 // With the use of enums, it is possible to reduce the number of bugs in your code.
 
 contract learnEnums {
-    enum frenchFriesSize {LARGE, MEDIUM, SMALL}
-    frenchFriesSize choice;
-    frenchFriesSize constant defaultChoice = frenchFriesSize.MEDIUM;
+   // enum frenchFriesSize {LARGE, MEDIUM, SMALL}
+   // frenchFriesSize choice;
+   // frenchFriesSize constant defaultChoice = frenchFriesSize.MEDIUM;
 
-    function setSmall() public {
-        choice = frenchFriesSize.SMALL;
+   // function setSmall() public {
+   //     choice = frenchFriesSize.SMALL;
+   // }
+
+   // function getChoice() public view returns(frenchFriesSize) {
+   //     return choice;
+   // }
+
+   // function getDefaultChoice() public view returns(uint) {
+   //     return uint(defaultChoice);
+   // }
+
+// Exercise:
+    enum shirtColor {RED, WHITE, BLUE}
+    shirtColor choice;
+    shirtColor constant defaultChoice = shirtColor.BLUE;
+
+    function setWhite() public {
+        choice = shirtColor.WHITE;
     }
 
-    function getChoice() public view returns(frenchFriesSize) {
+    function getChoice() public view returns(shirtColor) {
         return choice;
     }
 
-    function getDefaultChoice() public view returns(uint) {
-        return uint(defaultChoice);
+    function getDefaultChoice() public view returns(shirtColor) {
+        return defaultChoice;
     }
+
+
+
 }
