0c1409c1c087124d1e9fa2d686930423e671b83e
Interstellar-x8 <interstellar.x8@gmail.com>
2025-06-19 13:13:05 -0400

    mod.

--- learningSolidity/cryptographicFunctions.sol
+++ learningSolidity/cryptographicFunctions.sol
@@ -11,17 +11,38 @@ keccak256(bytes memory) returns (bytes32) - computes the Keccak-256 hash of the 
 sha256(bytes memory) returns (bytes32) - computes the SHA-256 hash of the input.
 
 ripemd160(bytes memory) returns (bytes20) - compute RIPEMD-160 hash of the input.
-
 */
 
+// Oracle dynamic feeds are basically databases that are feeding live information.
+
+contract Oracle {
+    address admin; 
+    uint public rand;
+
+    constructor() public {
+        admin = msg.sender;
+    }
+
+    function feedRand(uint _rand) external {
+        require(msg.sender == admin);
+        rand = _rand;
+    }
+}
+
 contract GenerateRandomNumber {
+
+    Oracle oracle;
+
+    constructor(address oracleAddress) {
+        oracle = Oracle(oracleAddress);
+    }
 
     // modulo (%) - so by computing against the remainder we will be able to produce a random number within a range.
 
     function randMod(uint range) external view returns(uint) {
         // grab information from the blockchain randomly to generate random numbers - we need something dynamically changing.
         // abi.encodePacked concatenates arguments nicely.
-        return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))) % range; 
+        return uint(keccak256(abi.encodePacked(oracle.rand, block.timestamp, block.difficulty, msg.sender))) % range; 
     }
 
 }
