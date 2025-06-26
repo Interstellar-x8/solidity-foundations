519d52ce16aab41b2b3202a49dac561ca5c6d2ad
Interstellar-x8 <interstellar.x8@gmail.com>
2025-06-26 07:31:20 -0400

    add

--- learningSolidity/advancedErrorHandling.sol
+++ learningSolidity/advancedErrorHandling.sol
@@ -3,6 +3,7 @@ pragma solidity >= 0.7.0 < 0.9.0;
 
 contract LearnErrorHandling {
     bool public sunny = true;
+    bool public umbrella = false;
     uint finalCalc = 0;
 
     function solarCalc() public {
@@ -13,6 +14,18 @@ contract LearnErrorHandling {
     // machine that controls the weather. 
     function weatherChanger() public {
         sunny = !sunny;
+    }
+
+    function getCalc() public view returns(uint) {
+        return finalCalc;
+    }
+
+    function bringUmbrella() public {
+        if(!sunny) {
+            umbrella = true;
+        } else {
+        revert('No need to bring an umbrella today!');
+        }
     }
 
 }
